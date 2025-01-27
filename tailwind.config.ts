f3e33a56d4368e5b467cdf8b5e6b5599ccaa79cb
sb57795 <134415146+sb57795@users.noreply.github.com>
2025-01-27 12:24:06 +0300

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,15 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{js,ts,jsx,tsx}",
+    "./compon/*.{js,ts,jsx,tsx}",
+    "./components/ui/*.{js,ts,jsx,tsx}"
+    
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
+
+
